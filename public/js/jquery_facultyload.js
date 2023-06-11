4b7dbc5cbefed3291972da1da7f0669cf7d42409
pakits1582 <pakits1582@gmail.com>
2023-06-11 14:00:20 +0800

    other assign

--- public/js/jquery_facultyload.js
+++ public/js/jquery_facultyload.js
@@ -60,5 +60,99 @@ $(function(){
         
         //console.log(postData);
         returnFacultyload(postData);
-    })
+    });
+
+    $(document).on("click", "#other_assignments", function(){
+        $.ajax({
+            type: "GET",
+            url: "/facultyloads/otherassignments",
+            success: function(data){
+                console.log(data);
+                $('#ui_content').html(data);
+                $("#other_assignments_modal").modal('show');
+
+                $("#instructor_id").select2({
+                    dropdownParent: $("#other_assignments_modal")
+                });
+            }
+        });
+    });
+
+    function returnOtherAssignments(period_id, instructor_id)
+    {
+        if(period_id && instructor_id)
+        {
+            $.ajax({
+                type: "POST",
+                url:  "/facultyloads/saveotherassignment",
+                dataType: 'json',
+                data:  postData,
+                cache: false, 
+                success: function(response){
+                    console.log(response);
+                    $("#confirmation").dialog('close');
+                    $(".errors").html('');
+    
+                    if(response.success == false)
+                    {
+                        showError(response.message);
+                    }else{
+                        showSuccess(response.message);
+                        //return other assignments
+                    }
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            });
+        }
+    }
+
+    $(document).on("submit", "#form_otherassignments", function(e){
+        var postData = $("#form_otherassignments").serializeArray();
+
+        $.ajax({
+            type: "POST",
+            url:  "/facultyloads/saveotherassignment",
+            dataType: 'json',
+            data:  postData,
+            cache: false, 
+            beforeSend: function() {
+                $("#confirmation").html('<div class="confirmation"></div><div class="ui_title_confirm">Loading Request</div><div class="message">Saving Transaction<br><div clas="mid"><img src="images/31.gif" /></div></div>').dialog({
+                    show: 'fade',
+                    resizable: false,	
+                    width: 350,
+                    height: 'auto',
+                    modal: true,
+                    buttons: false
+                });
+                $(".ui-dialog-titlebar").hide();
+            },
+            success: function(response){
+                console.log(response);
+                $("#confirmation").dialog('close');
+                $(".errors").html('');
+
+                if(response.success == false)
+                {
+                    showError(response.message);
+                }else{
+                    showSuccess(response.message);
+                    //return other assignments
+                }
+            },
+            error: function (data) {
+                console.log(data);
+                $("#confirmation").dialog('close');
+                var errors = data.responseJSON;
+                if ($.isEmptyObject(errors) == false) {
+                    showError('Something went wrong! System encountered some errors please check entries!');
+                    $.each(errors.errors, function (key, value) {
+                        $('#error_' + key).html('<p class="text-danger text-xs mt-1 errors">'+value+'</p>');
+                    });
+                }
+            }
+        });
+        e.preventDefault();
+    });
 });
