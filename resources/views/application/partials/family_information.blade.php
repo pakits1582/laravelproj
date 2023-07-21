1b899a46709fbcfa2f6e53b47a3744b072996576
pakits1582 <pakits1582@gmail.com>
2023-07-21 21:07:48 +0800

    appli form

--- resources/views/application/partials/family_information.blade.php
+++ resources/views/application/partials/family_information.blade.php
@@ -19,16 +19,25 @@
                             </div>
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Name</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control text-uppercase" id="keyword">
+                                    <label for="father_name" class="m-0 font-weight-bold text-primary">Name</label>
+                                    <input type="text" name="father_name" placeholder="" class="form-control text-uppercase" id="father_name" maxlength="255" minlength="2">
+                                    @error('father_name')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Contact Number</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="father_contactno" class="m-0 font-weight-bold text-primary">Contact Number</label>
+                                    <input type="text" name="father_contactno" placeholder="" class="form-control text-uppercase" id="father_contactno" minlength="4" maxlength="30">
+                                    @error('father_contactno')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Home Address</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="father_address" class="m-0 font-weight-bold text-primary">Home Address</label>
+                                    <input type="text" name="father_address" placeholder="" class="form-control text-uppercase" id="father_address" maxlength="255" minlength="2">
+                                    @error('father_address')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -38,22 +47,31 @@
                                     <h4 class="mb-2 font-weight-bold text-success">Mother</h4>
                                 </div>
                                 <div class="col-lg-7 col-sm-7">
-                                    <label for="confirmation_yes"  class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="confirmation" value="1" id="confirmation_yes" checked> Living </label>
-                                    <label for="confirmation_no"  class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="confirmation" value="2" id="confirmation_no"> Deceased </label>
+                                    <label for="motheralive"  class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="mother_alive" value="1" id="motheralive" checked> Living </label>
+                                    <label for="motherdeceased"  class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="mother_alive" value="2" id="motherdeceased"> Deceased </label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Name</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="mother_name" class="m-0 font-weight-bold text-primary">Name</label>
+                                    <input type="text" name="mother_name" placeholder="" class="form-control text-uppercase" id="mother_name" minlength="2" maxlength="255">
+                                    @error('mother_name')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Contact Number</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="mother_contactno" class="m-0 font-weight-bold text-primary">Contact Number</label>
+                                    <input type="text" name="mother_contactno" placeholder="" class="form-control" id="mother_contactno" minlength="2" maxlength="30">
+                                    @error('mother_contactno')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Home Address</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="mother_address" class="m-0 font-weight-bold text-primary">Home Address</label>
+                                    <input type="text" name="mother_address" placeholder="" class="form-control text-uppercase" id="mother_address" minlength="2" maxlength="255">
+                                    @error('mother_address')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -65,32 +83,53 @@
                             <h4 class="mb-2 font-weight-bold text-success">Guardian/Spouse (If married)</h4>
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Name</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_name" class="m-0 font-weight-bold text-primary">Name</label>
+                                    <input type="text" name="guardian_name" placeholder="" class="form-control text-uppercase" id="guardian_name" minlength="2" maxlength="255">
+                                    @error('guardian_name')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Relationship</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_relationship" class="m-0 font-weight-bold text-primary">Relationship</label>
+                                    <input type="text" name="guardian_relationship" placeholder="" class="form-control text-uppercase" id="guardian_relationship" minlength="2" maxlength="100">
+                                    @error('guardian_relationship')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Contact Number</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_contactno" class="m-0 font-weight-bold text-primary">Contact Number</label>
+                                    <input type="text" name="guardian_contactno" placeholder="" class="form-control text-uppercase" id="guardian_contactno" minlength="2" maxlength="30">
+                                    @error('guardian_contactno')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Home Address</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_address" class="m-0 font-weight-bold text-primary">Home Address</label>
+                                    <input type="text" name="guardian_address" placeholder="" class="form-control text-uppercase" id="guardian_address" minlength="2" maxlength="255">
+                                    @error('guardian_address')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Occupation</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_occupation" class="m-0 font-weight-bold text-primary">Occupation</label>
+                                    <input type="text" name="guardian_occupation" placeholder="" class="form-control text-uppercase" id="guardian_occupation" minlength="2" maxlength="255">
+                                    @error('guardian_occupation')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Company/Employer</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_employer" class="m-0 font-weight-bold text-primary">Company/Employer</label>
+                                    <input type="text" name="guardian_employer" placeholder="" class="form-control text-uppercase" id="guardian_employer" minlength="2" maxlength="255">
+                                    @error('guardian_employer')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Company Address</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="guardian_employer_address" class="m-0 font-weight-bold text-primary">Company Address</label>
+                                    <input type="text" name="guardian_employer_address" placeholder="" class="form-control text-uppercase" id="guardian_employer_address" minlength="2" maxlength="255">
+                                    @error('guardian_employer_address')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                
                             </div>
@@ -103,24 +142,39 @@
                             <h4 class="mb-2 font-weight-bold text-success">Work Details (If applicant is working)</h4>
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Present Occupation</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="occupation" class="m-0 font-weight-bold text-primary">Present Occupation</label>
+                                    <input type="text" name="occupation" placeholder="" class="form-control text-uppercase" id="occupation" minlength="2" maxlength="255">
+                                    @error('occupation')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Company/Employer</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="employer" class="m-0 font-weight-bold text-primary">Company/Employer</label>
+                                    <input type="text" name="employer" placeholder="" class="form-control text-uppercase" id="employer" minlength="2" maxlength="255">
+                                    @error('employer')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Company Address</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="employer_address" class="m-0 font-weight-bold text-primary">Company Address</label>
+                                    <input type="text" name="employer_address" placeholder="" class="form-control text-uppercase" id="employer_address" minlength="20" maxlength="255">
+                                    @error('employer_address')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Company Contact No.</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="employer_contact" class="m-0 font-weight-bold text-primary">Company Contact No.</label>
+                                    <input type="text" name="employer_contact" placeholder="" class="form-control text-uppercase" id="employer_contact" minlength="2" maxlength="30">
+                                    @error('employer_contact')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <label for="period_id" class="m-0 font-weight-bold text-primary">Years of Service</label>
-                                    <input type="text" name="keyword" placeholder="" class="form-control" id="keyword">
+                                    <label for="occupation_years" class="m-0 font-weight-bold text-primary">Years of Service</label>
+                                    <input type="text" name="occupation_years" placeholder="" class="form-control" id="occupation_years" minlength="1" maxlength="2">
+                                    @error('occupation_years')
+                                        <p class="text-danger text-xs mt-1">{{$message}}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
