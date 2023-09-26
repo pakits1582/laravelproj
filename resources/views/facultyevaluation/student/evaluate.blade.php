28ecb2715516cd01c2b86eb42631e3828d08bd86
pakits1582 <pakits1582@gmail.com>
2023-09-26 21:22:33 +0800

    evalclass

--- resources/views/facultyevaluation/student/evaluate.blade.php
+++ resources/views/facultyevaluation/student/evaluate.blade.php
@@ -102,18 +102,30 @@
                             <div class="col-md-3 text-black"><u>Sometimes</u> - <i>Once in a while</i></div>
                             <div class="col-md-3 text-black"><u>Never</u> - <i>Not at all</i></div>
                         </div>
+                        @php
+                            $choice1 = 'Always';
+                            $choice2 = 'Often';
+                            $choice3 = 'Sometimes';
+                            $choice4 = 'Never';
+                        @endphp
                     @else
                         <div class="row">
                             <div class="col-md-3 text-black"><u>Always</u></div>
                             <div class="col-md-3 text-black"><u>Sometimes</u></div>
                             <div class="col-md-3 text-black"><u>Rarely</div>
-                            <div class="col-md-3 text-black"><u>No oppurtunity to observe</u></div>
+                            <div class="col-md-3 text-black"><u>No opportunity to observe</u></div>
                         </div>
+                        @php
+                            $choice1 = 'Always';
+                            $choice2 = 'Sometimes';
+                            $choice3 = 'Rarely';
+                            $choice4 = 'No opportunity to observe';
+                        @endphp
                     @endif
                     <h6 class="my-3 font-weight-bold text-black"><i>Select radio button which corresponds to the rating of your instructor.</i></h6>
                     @if ($evaluate_class['questions'] !== null && count($evaluate_class['questions']) > 0)
                         @foreach ($evaluate_class['questions'] as $question)
-                            <h4 class="text-primary mt-2">{{ $question['category'] }}</h4>
+                            <h4 class="text-primary mt-2">&bull; {{ $question['category'] }}</h4>
                             @foreach ($question['subcategory'] as $subcategory)
                                 <h5 class="text-success font-italic">{{ $subcategory['subcategory'] }}</h5>
                                 @foreach ($subcategory['group'] as $group)
@@ -125,20 +137,80 @@
                                                     {{ $loop->iteration }}. {{ $question['question'] }}
                                                 </div>
                                             </div>
-                                            <div class="row">
-                                                <div class="col-md-3 text-black mid"><u>Always</u> - <i>All the time</i></div>
-                                                <div class="col-md-3 text-black mid"><u>Often</u> - <i>Most of the time</i></div>
-                                                <div class="col-md-3 text-black mid"><u>Sometimes</u> - <i>Once in a while</i></div>
-                                                <div class="col-md-3 text-black mid"><u>Never</u> - <i>Not at all</i></div>
+                                            <div class="row mb-2">
+                                                <div class="col-md-3 mid">
+                                                    <label for="choice1_{{ $question['id'] }}" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="choice[{{ $question['id'] }}]" value="1" id="choice1_{{ $question['id'] }}"> {{ $choice1 }} </label>
+                                                </div>
+                                                <div class="col-md-3 mid">
+                                                    <label for="choice2_{{ $question['id'] }}" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="choice[{{ $question['id'] }}]" value="2" id="choice2_{{ $question['id'] }}"> {{ $choice2 }} </label>
+                                                </div>
+                                                <div class="col-md-3 mid">
+                                                    <label for="choice3_{{ $question['id'] }}" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="choice[{{ $question['id'] }}]" value="3" id="choice3_{{ $question['id'] }}"> {{ $choice3 }} </label>
+                                                </div>
+                                                <div class="col-md-3 mid">
+                                                    <label for="choice4_{{ $question['id'] }}" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="choice[{{ $question['id'] }}]" value="4" id="choice4_{{ $question['id'] }}"> {{ $choice4 }} </label>
+                                                </div>
                                             </div>
                                         
                                     @endforeach
                                 @endforeach
                             @endforeach
                         @endforeach
+                        <h4 class="text-primary mt-2">&bull; As a whole, how do you rate your instructor's performance?</h4>
+                        <div class="row mb-2">
+                            <div class="col-md-3 mid">
+                                <label for="overallrate1" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="overallrate" value="1" id="overallrate1"> Outstanding </label>
+                            </div>
+                            <div class="col-md-3 mid">
+                                <label for="overallrate2" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="overallrate" value="2" id="overallrate2"> Very Good </label>
+                            </div>
+                            <div class="col-md-3 mid">
+                                <label for="overallrate3" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="overallrate" value="3" id="overallrate3"> Good </label>
+                            </div>
+                            <div class="col-md-3 mid">
+                                <label for="overallrate4" class="m-0 font-weight-bold text-primary"><input type="radio" class="" name="overallrate" value="4" id="overallrate4"> Fair </label>
+                            </div>
+                        </div>
                     @else
                         <h3 class="text-danger mid m-3">No survey questions to be displayed!</h3>
                     @endif
+                </div>
+            </div>
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <div class="row align-items-center">
+                        <div class="col-md-7">
+                            <h1 class="h3 text-800 text-primary mb-0">Comments</h1>
+                        </div>
+                        <div class="col-md-5 right"></div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="font-italic text-info">Note: Please choose words carefully and ensure that your language is respectful, professional, and appropriate for our institutional setting.</p>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="overallrate1" class="m-0 font-weight-bold text-primary">Strong Points</label>
+                            <textarea name="" class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="overallrate1" class="m-0 font-weight-bold text-primary">Weak Points</label>
+                            <textarea name="" class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="overallrate1" class="m-0 font-weight-bold text-primary">Suggestions for Improvement</label>
+                            <textarea name="" class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <label for="overallrate1" class="m-0 font-weight-bold text-primary">Other Student Services</label>
+                            <textarea name="" class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
                 </div>
             </div>
         @endif
